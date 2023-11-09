88c9cdc22fc5202755d4f28cdb6d4127f9a7a8ea
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-09 11:58:54 +0000

    login buttons

---
+++ LitvaKebabs/Components/LoginForm.razor.cs
@@ -0,0 +1,31 @@
+﻿using LitvaKebabs.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace LitvaKebabs.Components
+{
+    public partial class LoginForm
+    {
+        [Parameter]
+        public string LoginUser { get; set; }
+
+        public Login? Model { get; set; }
+
+        protected override void OnInitialized() => Model ??= new();
+
+        private void Submit()
+        {
+            if(LoginUser != "customer" || LoginUser != "staff")
+            {
+                throw new Exception("ERROR: Login user has not been specified. Should either be 'customer' or 'staff'.");
+            }
+            else if (LoginUser == "customer")
+            {
+                navigationManager.NavigateTo("/customer-portal");
+            }
+            else if (LoginUser == "staff")
+            {
+                navigationManager.NavigateTo("/staff-portal");
+            }
+        }
+    }
+}
---
+++ LitvaKebabs/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LitvaKebabs.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Username is required!")]
+        [EmailAddress(ErrorMessage = "Your username is your email address.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required!")]
+        [StringLength(8, ErrorMessage = "Password is too short.")]
+        public string Password { get; set; }
+    }
+}
