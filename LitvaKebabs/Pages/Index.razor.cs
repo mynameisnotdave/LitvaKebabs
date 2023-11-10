09cf8208ee6bd99432a6a023118b35495adc5a0b
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-10 09:58:05 +0000

    login form should not appear until button is clicked

---
+++ LitvaKebabs/Pages/Index.razor.cs
@@ -0,0 +1,8 @@
+﻿namespace LitvaKebabs.Pages
+{
+    public partial class Index
+    {
+        bool ShowCustomerComponent { get; set; } = false;
+        bool ShowStaffComponent { get; set; } = false;
+    }
+}
