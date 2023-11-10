8cfbab6ffd0b86b387297ed561ae5503f1f645f5
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-10 09:58:26 +0000

    take a load of stuff out order menu root razor code behind

--- LitvaKebabs/Pages/OrderMenuRoot.razor.cs
+++ LitvaKebabs/Pages/OrderMenuRoot.razor.cs
@@ -1,43 +1,27 @@
 ﻿using Microsoft.AspNetCore.Components;
+using LitvaKebabs.Util;
 
 namespace LitvaKebabs.Pages
 {
     public partial class OrderMenuRoot
     {
-        public OrderMenuRoot()
+
+        public Dictionary<string, decimal> MenuItems = Menu.MenuItems;
+  
+        public Dictionary<string, decimal> SauceItems = Menu.SauceItems;
+
+        public List<ReceiptItems> Receipt = new List<ReceiptItems>();
+
+        // Receipt Type
+        public class ReceiptItems
         {
-            MenuItems = _menuItems;
-            SauceItems = _sauceItems;
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; } = 0;
         }
 
-        [Parameter]
-        public Dictionary<string, decimal> MenuItems { get; set; }
-        [Parameter]
-        public Dictionary<string, decimal> SauceItems { get; set; }
+        // Receipt State
+        
 
-        private Dictionary<string, decimal> _menuItems = new Dictionary<string, decimal>()
-        {
-            { "CHICKEN DÖNER WRAP", 10.95M },
-            { "CHICKEN SHISH WRAP", 11.95M },
-            { "LAMB YAPRAK WRAP", 12.95M },
-            { "LAMB DÖNER WRAP", 10.95M },
-            { "LAMB SHISH WRAP",12.95M },
-            { "MIX SHISH WRAP",12.95M },
-            { "LAMB KÖFTE WRAP",11.95M },
-            { "FALAFEL, RED PEPPER, RED ONION & HUMMUS WRAP", 10.95M }
-        };
-
-        private Dictionary<string, decimal> _sauceItems = new Dictionary<string, decimal>()
-        {
-
-            { "MINT SAUCE", 0.49M },
-            { "MAYO", 0.49M },
-            { "HUMMUS", 0.99M },
-            { "SALAD CREAM", 0.49M },
-            { "TAHINI", 0.99M },
-            { "LEMON AND LIME SAUCE", 0.99M },
-            { "AJVAR", 1.99M },
-            { "KAJMAK", 2.49M }
-        };
     }
 }
