d3c554f35586497e430244ce290e6c6ed6133694
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-13 10:40:59 +0000

    there! is! no! need! for! constant! exclamation! marks!

--- LitvaKebabs/Models/Login.cs
+++ LitvaKebabs/Models/Login.cs
@@ -4,10 +4,10 @@ namespace LitvaKebabs.Models
 {
     public class Login
     {
-        [Required(ErrorMessage = "Username is required!")]
+        [Required(ErrorMessage = "Username is required")]
         [EmailAddress(ErrorMessage = "Your username is your email address.")]
         public string Username { get; set; }
-        [Required(ErrorMessage = "Password is required!")]
+        [Required(ErrorMessage = "Password is required")]
         [StringLength(8, ErrorMessage = "Password is too short.")]
         public string Password { get; set; }
     }
