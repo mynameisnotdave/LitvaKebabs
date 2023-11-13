ac72aede47a59555cea877977f1546d19e62d353
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-13 10:41:32 +0000

    not sure what i am doing part 2

--- LitvaKebabs/Components/ReceiptComponent.razor.cs
+++ LitvaKebabs/Components/ReceiptComponent.razor.cs
@@ -1,10 +1,24 @@
-﻿using LitvaKebabs.Pages;
+﻿using LitvaKebabs.Models;
+using LitvaKebabs.Pages;
+using LitvaKebabs.Util;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace LitvaKebabs.Components
 {
     public partial class ReceiptComponent
     {
+        private List<KebabMenu> GetMenuItem() {
+            Dictionary<string, decimal> keyValuePairs = Menu.MenuItems;
+            List<KebabMenu> kebabMenus = new List<KebabMenu>()
+            {
+                new KebabMenu(){Item=$"{keyValuePairs.Keys.FirstOrDefault()}",Price=keyValuePairs.Values.FirstOrDefault()}
+            };
+            return kebabMenus;
+        }
+        
+
         OrderButton orderButton = new OrderButton();
+        
         private decimal TotalAmount()
         {
             return 0;
