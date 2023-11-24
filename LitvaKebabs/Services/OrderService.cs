3360d179376db9b5645064fcb5eb11419fc9ac9e
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-24 10:38:01 +0000

    create order service

--- LitvaKebabs/Services/OrderService.cs
+++ LitvaKebabs/Services/OrderService.cs
@@ -1,6 +1,33 @@
-﻿namespace LitvaKebabs.Services
+﻿using LiteDB;
+using LitvaKebabs.Models;
+
+namespace LitvaKebabs.Services
 {
     public class OrderService
     {
+        private ILiteCollection<Order> _orderTable;
+
+        public OrderService()
+        {
+            using (LiteDatabase database = new LiteDatabase(@"./mydatabase"))
+            {
+                _orderTable = database.GetCollection<Order>("Orders");
+            }
+        }
+
+        public Order GetOrder(int id)
+        {
+            return _orderTable.Query().Where(x => x.Id == id).First();
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            _orderTable.Update(order);
+        }
+
+        public void DeleteOrder(int id)
+        {
+            _orderTable.Delete(id);
+        }
     }
 }
