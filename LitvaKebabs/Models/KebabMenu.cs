8f5c47a70b0be0b2ef9673e23afeea60c19674fe
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-27 11:43:35 +0000

    these models are no longer needed

--- LitvaKebabs/Models/KebabMenu.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LitvaKebabs.Models
-{
-    public class KebabMenu
-    {
-        public string Item { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-    }
-}
--- LitvaKebabs/Models/SauceMenu.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LitvaKebabs.Models
-{
-    public class SauceMenu
-    {
-        public string Sauce {  get; set; } = string.Empty;
-        public decimal Price { get; set; }
-    }
-}
