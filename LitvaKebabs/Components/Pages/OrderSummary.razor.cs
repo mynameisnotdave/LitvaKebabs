bbac28d2a9f2b729ad567b85c15fb0b2a37c5350
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-28 11:30:43 +0000

    order summary almost shows the order info...in the form of litvakebabs.models.menuitem

---
+++ LitvaKebabs/Components/Pages/OrderSummary.razor.cs
@@ -0,0 +1,21 @@
+﻿using LitvaKebabs.Services;
+using LitvaKebabs.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace LitvaKebabs.Components.Pages
+{
+    public partial class OrderSummary
+    {
+        OrderService _orderService = new();
+        Order order = new();
+
+        [SupplyParameterFromQuery(Name = "id")]
+        [Parameter]
+        public string OrderId { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
+    }
+}
