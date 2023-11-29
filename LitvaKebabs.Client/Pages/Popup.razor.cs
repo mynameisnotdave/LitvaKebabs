7ab4601d02d6a06b91d0fd4021e355fbae61a5df
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-29 08:48:56 +0000

    new popup component

---
+++ LitvaKebabs.Client/Pages/Popup.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LitvaKebabs.Client.Pages
+{
+    public partial class Popup
+    {
+        [Parameter]
+        public bool IsVisible { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> IsVisibleChanged { get; set; }
+
+        [Parameter]
+        public string? HeaderText { get; set; }
+
+        [Parameter]
+        public string? BodyText { get; set; }
+
+        public void Show(string bodyText, string headerText = "")
+        {
+            HeaderText = headerText;
+            BodyText = bodyText;
+            IsVisible = true;
+            StateHasChanged();
+        }
+
+        private void Close()
+        {
+            HeaderText = string.Empty;
+            BodyText = string.Empty;
+            IsVisible = false;
+            StateHasChanged();
+        }
+    }
+}
