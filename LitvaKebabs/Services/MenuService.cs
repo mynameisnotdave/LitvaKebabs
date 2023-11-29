3cf45aacf3ff42adefad428dc92ade721f06e660
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-29 09:41:19 +0000

    add that to service

--- LitvaKebabs/Services/MenuService.cs
+++ LitvaKebabs/Services/MenuService.cs
@@ -13,82 +13,98 @@ namespace LitvaKebabs.Services
 
             _menuItemTable = database.GetCollection<MenuItem>("menuItems");
             var hasItems = _menuItemTable.Query().ToList().Count != 0;
+            int id = 0;
             if (!hasItems)
             {
                 _menuItemTable.InsertBulk(new List<MenuItem>()
                     {
                         new()
                         {
+                            Id = id++,
                             Name = "\"CHICKEN DÖNER WRAP\"",
                             Price = 10.95M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "CHICKEN SHISH WRAP",
                             Price = 11.95M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "LAMB YAPRAK WRAP",
                             Price = 12.95M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "LAMB DÖNER WRAP",
                             Price = 10.95M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "LAMB SHISH WRAP",
                             Price = 12.95M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "MIX SHISH WRAP",
                             Price = 12.95M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "LAMB KÖFTE WRAP",
                             Price = 11.95M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "MINT SAUCE",
                             Price = 0.49M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "MAYO",
                             Price = 0.49M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "HUMMUS",
                             Price = 0.99M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "SALAD CREAM",
                             Price = 0.99M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "TAHINI",
                             Price = 0.99M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "LEMON AND LIME SAUCE",
                             Price = 0.99M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "AJVAR",
                             Price = 1.99M
                         },
                         new()
                         {
+                            Id = id++,
                             Name = "KAJMAK",
                             Price = 2.49M
                         }
