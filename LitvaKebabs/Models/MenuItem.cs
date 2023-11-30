c13fff88f622d807f959aa6b8f498065a4f83074
Aidan Fell <Aidan.Fell@hcmercury.onmicrosoft.com>
2023-11-30 08:45:50 +0000

    id in poco is now of type ObjectId

--- LitvaKebabs/Models/MenuItem.cs
+++ LitvaKebabs/Models/MenuItem.cs
@@ -1,8 +1,10 @@
-﻿namespace LitvaKebabs.Models
+﻿using LiteDB;
+
+namespace LitvaKebabs.Models
 {
     public class MenuItem
     {
-        public int Id { get; set; }
+        public ObjectId Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
 
