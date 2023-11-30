de0e1a8c8e86a10727e760250dde098284a4d077
Aidan Fell <islandmonkeee@gmail.com>
2023-11-30 10:49:10 +0000

    maybe this dialog will work

---
+++ LitvaKebabs/Components/AddMenuItemDialog.razor.cs
@@ -0,0 +1,41 @@
+﻿using LitvaKebabs.Models;
+using LitvaKebabs.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace LitvaKebabs.Components
+{
+    public partial class AddMenuItemDialog
+    {
+        public MenuItem MenuItem { get; set; } = new MenuItem();
+
+        [Inject]
+        public IMenuService MenuService { get; set; }
+
+        public bool ShowDialog { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> CloseEventCallback { get; set; }
+        public void Show()
+        {
+            ResetDialog();
+            ShowDialog = true;
+            StateHasChanged();
+        }
+        public void Close()
+        {
+            ShowDialog = false;
+            StateHasChanged();
+        }
+        private void ResetDialog()
+        {
+            MenuItem = new MenuItem { };
+        }
+        protected async Task HandleValidSubmit()
+        {
+            MenuService.UpsertMenuItem(MenuItem);
+            ShowDialog = false;
+            await CloseEventCallback.InvokeAsync(true);
+            StateHasChanged();
+        }
+    }
+}
