d5da4133de1c772c011a25681f2337e7333a1ba5
Aidan Fell <islandmonkeee@gmail.com>
2024-03-07 11:56:38 +0000

    customer service

---
+++ LitvaKebaabs/LitvaKebabs/Services/CustomerService.cs
@@ -0,0 +1,46 @@
+﻿
+using LitvaKebabs.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LitvaKebabs.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private LitvaKebabsContext _dbcontext;
+
+        public CustomerService(LitvaKebabsContext dbcontext) 
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task AddCustomerAsync(Customer customer)
+        {
+            await _dbcontext.Customer.AddAsync(customer);
+            await _dbcontext.SaveChangesAsync();
+        }
+
+        public async Task DeleteCustomerAsync(Customer customer)
+        {
+            _dbcontext.Customer.Remove(customer);
+            await _dbcontext.SaveChangesAsync();
+        }
+
+        public async Task<List<Customer>> GetAllCustomersAsync(Customer customer)
+        {
+            List<Customer> customerList = await _dbcontext.Customer.ToListAsync();
+            return customerList;
+        }
+
+        public async Task<Customer> GetCustomerAsync(int customerId)
+        {
+            Customer customer = await _dbcontext.Customer.FindAsync(customerId) ?? new Customer();
+            return customer;
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            _dbcontext.Customer.Update(customer);
+            _dbcontext.SaveChanges();
+        }
+    }
+}
---
+++ LitvaKebaabs/LitvaKebabs/Services/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using LitvaKebabs.Models;
+
+namespace LitvaKebabs.Services
+{
+    public interface ICustomerService
+    {
+        public abstract Task AddCustomerAsync(Customer customer);
+
+        public void UpdateCustomer(Customer customer);
+
+        public abstract Task DeleteCustomerAsync(Customer customer);
+
+        public abstract Task<List<Customer>> GetAllCustomersAsync(Customer customer);
+
+        public abstract Task<Customer> GetCustomerAsync(int customerId);
+    }
+}
